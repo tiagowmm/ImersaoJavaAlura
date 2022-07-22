532e2011560aa42a2f8c2e2da4c5743b0853d6c2
tiago.menezes <tiago.menezes@bbts.com.br>
2022-07-22 18:37:15 -0300

    lendo a figurinha de uma url

--- src/main/java/br/com/tw/util/GeradoraDeFigurinhas.java
+++ src/main/java/br/com/tw/util/GeradoraDeFigurinhas.java
@@ -4,9 +4,8 @@ import javax.imageio.ImageIO;
 import javax.imageio.stream.FileImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 public class GeradoraDeFigurinhas {
 
@@ -15,7 +14,9 @@ public class GeradoraDeFigurinhas {
 
         // leitura da imagem
 
-        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+//        InputStream inputStream = new FileInputStream(new File("inputStream/filme.jpg"));
+        InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
         // cria nova imagem em memória com transparencia e com novo tamanho.
 
@@ -42,7 +43,6 @@ public class GeradoraDeFigurinhas {
 
         // escrever a nova imagem em um arquivo.
         ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
-
 
     }
 
